e2d2e1438417330dd79adc0652d590f6efaa4ce8
Lilia <excellencecenter001@gmail.com>
2024-04-03 21:06:18 +0400

    Alvard helped me and I am happy

--- client/Components/header_footer/header_footer.js
+++ client/Components/header_footer/header_footer.js
@@ -1,8 +1,8 @@
 function createHeader() {
-    const header = document.getElementById("header");
-    header.className = "header";
-    if (window.location.href.endsWith("index.html")) {
-      header.innerHTML = `
+  const header = document.getElementById("header");
+  header.className = "header";
+  if (window.location.href.endsWith("index.html")) {
+    header.innerHTML = `
           <div class="header1">
               <p class="signin_signup">
                   <a href="./Sign_in/signin.html"><span class="s1">Sign in</span></a>
@@ -10,34 +10,51 @@ function createHeader() {
               </p>
               <img class="lighting" src="Components/images/lighting.png">
           </div>`;
-    } else {
-      header.innerHTML = `
+  } else {
+    header.innerHTML = `
           <div class="header1">
               <a href="index.html"><span class="log_out">Log out</span></a>
-              <img class="lighting" src="Components/images/lighting.png">
+              <img class="lighting" src="../Components/images/lighting.png">
           </div>`;
-    }
+    //   header.innerHTML = `
+    //   <div class="header1">
+
+    //       <img class="lighting" src="Components/images/lighting.png">
+    //   </div>`;
   }
-  
-  function createFooter() {
-    const footer = document.getElementById("footer"); // Append footer directly to the body
-    footer.className = "footer";
+}
+
+function createFooter() {
+  const footer = document.getElementById("footer"); // Append footer directly to the body
+  footer.className = "footer";
+  if (window.location.href.endsWith("index.html")) {
     footer.innerHTML = `
-          <div class="rectangle">
-              <span class="contacts">Contact us</span>
-              <div class="icons">
-                  <img class="facebook" src="Components/images/facebook.png" alt="facebook">  
-                  <img class="youtube" src="Components/images/youtube.png" alt="youtube">
-                  <img class="instagram" src="Components/images/instagram.png" alt="instagram">
-                  <img class="telegram" src="Components/images/telegram.png" alt="telegram">
-              </div>
-          </div> `;
+            <div class="rectangle">
+                <span class="contacts">Contact us</span>
+                <div class="icons">
+                    <img class="facebook" src="Components/images/facebook.png" alt="facebook">  
+                    <img class="youtube" src="Components/images/youtube.png" alt="youtube">
+                    <img class="instagram" src="Components/images/instagram.png" alt="instagram">
+                    <img class="telegram" src="Components/images/telegram.png" alt="telegram">
+                </div>
+            </div> `;
+  } else {
+    footer.innerHTML = `
+            <div class="rectangle">
+                <span class="contacts">Contact us</span>
+                <div class="icons">
+                    <img class="facebook" src="../Components/images/facebook.png" alt="facebook">  
+                    <img class="youtube" src="../Components/images/youtube.png" alt="youtube">
+                    <img class="instagram" src="../Components/images/instagram.png" alt="instagram">
+                    <img class="telegram" src="../Components/images/telegram.png" alt="telegram">
+                </div>
+            </div> `;
   }
-  
-  if (document.getElementById("footer")) {
-    createFooter();
-  }
-  if (document.getElementById("header")) {
-    createHeader();
-  }
-  
+}
+
+if (document.getElementById("footer")) {
+  createFooter();
+}
+if (document.getElementById("header")) {
+  createHeader();
+}
